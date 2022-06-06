3e541d0db5a7be3722a6859277696a0ce341ff9e
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-05 22:52:15 -0500

    Se agrega el movimiento de la pelota

--- game.js
+++ game.js
@@ -5,12 +5,13 @@
         this.playing = false;
         this.game_over = false;
         this.bars = [];
-        this.ball = null
+        this.ball = null;
+        this.playing = false;
     }
 
     self.Board.prototype = {
         get elements() {
-            var elements = this.bars;
+            var elements = this.bars.map(function (bar) { return bar; });
             elements.push(this.ball);
             return elements;
         }
@@ -25,10 +26,17 @@
         this.speed_y = 0;
         this.speed_x = 3
         this.board = board;
+        this.direction = 1;
 
         board.ball = this;
         this.kind = "circle";
-    }
+    },
+        self.Ball.prototype = {
+            move: function () {
+                this.x += (this.speed_x * this.direction);
+                this.y += (this.speed_y * this.direction);
+            }
+        }
 })();
 (function () {
     self.Bar = function (x, y, width, height, board) {
@@ -77,8 +85,11 @@
             };
         },
         play: function () {
-            this.clean();
-            this.draw();
+            if (this.board.playing) {
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+            }
         }
     }
 
@@ -104,20 +115,24 @@ var bar = new Bar(20, 100, 40, 100, board);
 var bar_2 = new Bar(735, 100, 40, 100, board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
-var ball = new Ball(350,100,10,board);
+var ball = new Ball(350, 100, 10, board);
 
 document.addEventListener("keydown", function (ev) {
-    ev.preventDefault();
-    if (ev.keyCode == 38) {
+    if (ev.keyCode === 38) {
+        ev.preventDefault();
         bar_2.up();
-    } else if (ev.keyCode == 40) {
+    } else if (ev.keyCode === 40) {
+        ev.preventDefault();
         bar_2.down();
-    } else if (ev.keyCode == 87) {
+    } else if (ev.keyCode === 87) {
         ev.preventDefault();
         bar.up();
-    } else if (ev.keyCode == 83) {
+    } else if (ev.keyCode === 83) {
         ev.preventDefault();
         bar.down();
+    } else if (ev.keyCode === 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
 });
 
@@ -125,5 +140,5 @@ window.requestAnimationFrame(controller);
 
 function controller() {
     board_view.play();
-    window.requestAnimationFrame(controller);
+    requestAnimationFrame(controller);
 }
