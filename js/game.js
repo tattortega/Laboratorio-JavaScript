1aa4e38729d012bff5c04a0df2c21629c698dc57
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-05 21:53:53 -0500

    Se agregan las barras laterales en el tablero del juego

--- js/game.js
+++ js/game.js
@@ -1,37 +1,82 @@
-(function(){
-    self.Board = function(width, height){
+(function () {
+    self.Board = function (width, height) {
         this.width = width;
         this.height = height;
         this.playing = false;
         this.game_over = false;
         this.bars = [];
-        this.ball = null 
+        this.ball = null
     }
 
     self.Board.prototype = {
-        get elements(){
+        get elements() {
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
 })();
 
- (function(){
-     self.BoardView = function(canvas, board){
-         this.canvas = canvas;
-         this.canvas.width = board.width;
-         this.canvas.height = board.height;
-         this.board = board;
-         this.ctx = canvas.getContext("2d"); 
-     }
- })();
+(function () {
+    self.Bar = function (x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    }
 
- window.addEventListener("load",main);
+    self.Bar.prototype = {
+        down: function () {
 
-function main(){
-    var board = new Board(800,400);
+        },
+        up: function () {
+
+        }
+    }
+})();
+
+
+(function () {
+    self.BoardView = function (canvas, board) {
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
+    }
+
+    self.BoardView.prototype = {
+        draw: function () {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i];
+
+                draw(this.ctx, el);
+            }
+        }
+    }
+
+    function draw(ctx, element) {
+        if (element !== null && element.hasOwnProperty("kind")) {
+            switch (element.kind) {
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }
+    }
+})();
+
+window.addEventListener("load", main);
+
+function main() {
+    var board = new Board(800, 400);
+    var bar = new Bar(20, 100, 40, 100, board);
+    var bar = new Bar(735, 100, 40, 100, board);
     var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas,board);
+    var board_view = new BoardView(canvas, board);
+    board_view.draw();
 
 }
